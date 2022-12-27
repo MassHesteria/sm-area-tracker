95e674cc2bd6243e473339a39015b42a8b3dcbde
MassHesteria <brad.hester@gmail.com>
2022-12-27 09:29:36 -0600

    Better support for adding/removing settings.

--- src/settings.js
+++ src/settings.js
@@ -10,19 +10,42 @@ const defaultSettings = {
 function loadSettings() {
    try {
       let str = window.localStorage.getItem(appName);
+
+      // Found existing settings?
       if (str !== null && str != "") {
-         return JSON.parse(str);
+         let existing = JSON.parse(str);
+
+         // Add entries to existing settings using the defaults
+         // if new options were added.
+         for (const key in defaultSettings) {
+            if (!existing.hasOwnProperty(key)) {
+               existing[key] = defaultSettings[key];
+            }
+         }
+
+         return existing;
       }
    } catch (e) {
-      // do nothing
+      // Do nothing.
    }
    return defaultSettings;
 }
 
 function saveSettings(settings) {
    try {
+      // Make a copy of the keys on the current settings.
+      const keys = [...Object.keys(settings)];
+
+      // Remove any settings that are no longer supported.
+      keys.forEach((p) => {
+         if (!defaultSettings.hasOwnProperty(p)) {
+            delete settings[p];
+         }
+      });
+
+      // Save the settings to local storage.
       window.localStorage.setItem(appName, JSON.stringify(settings));
    } catch (e) {
-      // do nothing
+      // Do nothing.
    }
 }
